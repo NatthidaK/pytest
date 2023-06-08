26400694caa45a5850071d362a93e29266128948
NatthidaK <135940538+NatthidaK@users.noreply.github.com>
2023-06-08 17:17:42 +0700

    Update pycode.py

--- pycode.py
+++ pycode.py
@@ -1,35 +1,22 @@
-quit = False
-while quit == False:
-  print("Welcome to calculator.py")
-  print("your option are: \n")
-  print("1. Addition")
-  print("2. Subtraction")
-  print("3. Multiplicaton")
-  print("4. Division")
-  print("5. Quit calculator.py \n")
-  x = input("Choose your option: ")
-  if x == '1':
-    add = int(input("Add this: "))
-    adder = int(input("by this: "))
-    addans = add + adder
-    print(add ,"+" ,adder ,"=", addans," \n" )
-  elif x == '2':
-    sub = int(input("Subtract this: "))
-    subber = int(input("by this: "))
-    subans = sub - subber
-    print(sub ,"-" ,subber ,"=", subans," \n" )
-  elif x == '3':
-    mul =  int(input("Multiply this: "))
-    muller = int(input("by this: "))
-    mulans = mul * muller
-    print(mul ,"*" ,muller ,"=", mulans,  " \n")
-  elif x == '4':
-    div =  int(input("Divide this: "))
-    diver =  int(input("by this: "))
-    divans = div/diver
-    print(div ,"/" ,diver ,"=", divans, " \n")
-  elif x == '5':
-    quit =True
-    print("Thank you for using calculator.py! \n")
-  else :
-    print("Please input valid option!!! Try again.\n")
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
